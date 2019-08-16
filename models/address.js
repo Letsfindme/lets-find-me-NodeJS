9c9c53ec6ba061186a0a59a499a1c657cc385d62
Fadi NOUFAL <noufal4me@gmail.com>
2019-08-16 23:16:33 +0200

    Add model address

---
+++ models/address.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Address = sequelize.define('address', {
+  id: {
+    type: Sequelize.UUID,
+    allowNull: false,
+    primaryKey: true,
+    defaultValue: Sequelize.UUIDV4()
+  },
+  street: Sequelize.STRING,
+  city: Sequelize.STRING,
+  country: Sequelize.STRING,
+  postcode: Sequelize.INTEGER,
+});
+
+module.exports = Address;
