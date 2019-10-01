410b9f68ce60b23fc3f4e757adf5839dcb9c62e8
Fadi NOUFAL <noufal4me@gmail.com>
2019-10-01 21:18:59 +0200

    Add bd type

--- util/database.js
+++ util/database.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('letsfind', 'root', 'rootroot', {
-  dialect: 'mysql',
-  host: 'localhost'
+const sequelize = new Sequelize('letsfind', 'postgres', 'rootroot', {
+  dialect: 'postgres',
+  host: 'localhost',
+  port:'3306'
 });
 
 module.exports = sequelize;
