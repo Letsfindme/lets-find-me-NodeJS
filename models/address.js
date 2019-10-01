c0bb98ac186e64aa849b8e166939e7f4e39d773b
Fadi NOUFAL <noufal4me@gmail.com>
2019-10-01 21:15:54 +0200

    Create adresse model

--- models/address.js
+++ models/address.js
@@ -1,8 +1,9 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const sequelize = require('../util/database');
+const sequelize = require("../util/database");
+const User = require('./user');
 
-const Address = sequelize.define('address', {
+const Address = sequelize.define("address", {
   id: {
     type: Sequelize.UUID,
     allowNull: false,
@@ -12,7 +13,7 @@ const Address = sequelize.define('address', {
   street: Sequelize.STRING,
   city: Sequelize.STRING,
   country: Sequelize.STRING,
-  postcode: Sequelize.INTEGER,
+  postcode: Sequelize.INTEGER
 });
 
 module.exports = Address;
