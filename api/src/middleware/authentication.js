7d813d2e3c1cc475aa7a0297884537cc71b4fef2
Fadi NOUFAL <noufal4me@gmail.com>
2020-09-21 12:28:05 +0200

    Update user auth

--- api/src/middleware/authentication.js
+++ api/src/middleware/authentication.js
@@ -1,22 +1,47 @@
 // Imports
 import jwt from "jsonwebtoken";
 import serverConfig from "../config/server.json";
+import models from "../setup/models";
 
 // Authentication middleware
-module.exports = (request, response, next) => {
-  let authToken = request.headers.authorization;
+module.exports = async (req, res, next) => {
+  let authToken = req.headers.authorization;
   if (authToken && authToken !== null) {
     try {
-      const token = authToken.split(" ");
-      request.user = jwt.verify(token[1], serverConfig.secret);
+      if (!authToken) {
+        const error = new Error("No token!!");
+        throw error;
+      }
+      const token = authToken.split(" ")[1];
+      req.userTok = jwt.verify(token, serverConfig.secret);
+      
+      req.userId = req.userTok.userId;
+      req.user = await models.User.findByPk(req.userId, {
+        include: [
+          {
+            model: models.Role,
+            attributes: ["type"]
+          }
+        ]
+      });
+      if (!req.user) {
+        const error = new Error("No user found!!");
+        throw error;
+      }
     } catch (e) {
-      console.warn("Invalid token detected.");
-      const error = new Error("Not authenticated.");
-      error.statusCode = 401;
-      throw error;
+      //todo logs
+      console.log(e.message);
+      // error.statusCode = 401;
+      // throw error;
+      return next(e);
     }
   } else {
-    request.user = {};
+    req.user = {};
+    console.warn("No token detected.");
+    const error = new Error("Not authenticated.");
+    // error.statusCode = 401;
+    // throw error;
+    return next(error);
   }
   next();
 };
