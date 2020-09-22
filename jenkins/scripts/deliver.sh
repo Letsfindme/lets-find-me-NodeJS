ab3c1b3ea811ad1a1ab84a1f5909dce36a332081
Fadi NOUFAL <noufal4me@gmail.com>
2020-09-22 11:11:31 +0200

    Add nohuo for prod

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,21 +1,10 @@
-echo 'The following "npm" command builds your Node.js/React application for'
-echo 'production in the local "build" directory (i.e. within the'
-echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
-echo 'correctly bundles React in production mode and optimizes the build for'
-echo 'the best performance.'
+echo 'The following "npm" command buildsLetsFindNode application'
+echo '"/var/jenkins_home/workspace/LetsFindNode" directory),'
 set -x
 cd api
-npm start -d
+npm run start:prod
 set +x
 
-echo 'The following "npm" command runs your Node.js/React application in'
-echo 'development mode and makes the application available for web browsing.'
-echo 'The "npm start" command has a trailing ampersand so that the command runs'
-echo 'as a background process (i.e. asynchronously). Otherwise, this command'
-echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
-echo 'is followed by another command that retrieves the process ID (PID) value'
-echo 'of the previously run process (i.e. "npm start") and writes this value to'
-echo 'the file ".pidfile".'
 set -x
 # npm start &
 sleep 1
@@ -23,6 +12,4 @@ sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
-echo 'created your initial Pipeline as a Jenkinsfile.)'
+echo 'Visit http://localhost:1989 to see LetsFindNode application in action.'
