4f8fa6f253e15f286d181096c4e2a8bb45e29899
Fadi <noufal4@gmail.com>
2021-11-05 20:59:55 +0100

    database connection for Heroku

--- api/src/setup/database.js
+++ api/src/setup/database.js
@@ -9,21 +9,31 @@ import databaseConfig from "../config/database.json";
 // Load database config
 const databaseConfigEnv = databaseConfig[NODE_ENV];
 
+// Create new database connection for Heroku
+const connection = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
 // Create new database connection
-const connection = new Sequelize(
-  databaseConfigEnv.database,
-  databaseConfigEnv.username,
-  databaseConfigEnv.password,
-  {
-    host: databaseConfigEnv.host,
-    dialect: databaseConfigEnv.dialect,
-    logging: console.log,
-    port:databaseConfigEnv.port
-  }
-);
+// const connection = new Sequelize(
+//   databaseConfigEnv.database,
+//   databaseConfigEnv.username,
+//   databaseConfigEnv.password,
+//   {
+//     host: databaseConfigEnv.host,
+//     dialect: databaseConfigEnv.dialect,
+//     logging: console.log,
+//     port: databaseConfigEnv.port,
+//   }
+// );
 
 // Test connection
-console.info("SETUP - Connecting database...",databaseConfigEnv.port);
+console.info("SETUP - Connecting database...", databaseConfigEnv.port);
 
 connection
   // .sync({
@@ -33,8 +43,10 @@ connection
   .then(() => {
     console.info(chalk.green.inverse.bold("INFO - Database connected."));
   })
-  .catch(err => {
-    console.error(chalk.red.bgGreen.bold("ERROR - Unable to connect to the database:", err));
+  .catch((err) => {
+    console.error(
+      chalk.red.bgGreen.bold("ERROR - Unable to connect to the database:", err)
+    );
   });
 
 export default connection;
