baa03d956064aaa68782dccc7e6cf39d234a2d64
Fadi <noufal4@gmail.com>
2021-11-06 16:00:39 +0100

    Revert "remove importe json"
    This reverts commit 3fa048f3b8f35b55467692ae3ec5a9200bf8a398.

--- src/middleware/is-admin.js
+++ src/middleware/is-admin.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 import models from "../setup/models";
-// import serverConfig from "../config/server.json";
+import serverConfig from "../config/server.json";
 
 module.exports = async (req, res, next) => {
   try {
